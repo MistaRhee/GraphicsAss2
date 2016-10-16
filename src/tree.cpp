cf2e267a021f8fac892c76fe477b5be3d5971212
MistaRhee <jhuangmystery@gmail.com>
2016-10-16 20:37:32 +1100

    Temp Commit

--- src/tree.cpp
+++ src/tree.cpp
@@ -12,7 +12,7 @@ namespace __game__ {
     void cTree::addPoints(double h) {
         /* Trees have radius from 2 units -> 5 units */
         double radius = (int)rand()%3+2;
-        /* Estimate base with 64 points */
+        /* Estimate base with 64 points, Don't need to draw out the base circles, just the cylinders. */
         for (int i = 0; i < 64; i++) {
             double theta = (2 * M_PI) * i / 64;
             /* Points go from 0 -> height*/
@@ -20,7 +20,18 @@ namespace __game__ {
     }
 
     void cTree::render() {
+        glBegin(GL_QUAD_STRIP);
+        {
+            for (int i = 0; i < 128; i++) {
 
+            }
+        }
+        glEnd();
+        glBegin(GL_TRIANGLE_STRIP);
+        {
+
+        }
+        glEnd();
     }
 
 }
